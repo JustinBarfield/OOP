3886a3920d0e6602712abe3efedddd87b37c94dc
JustinBarfield <122563454+JustinBarfield@users.noreply.github.com>
2023-09-12 22:57:46 +0000

    outline

---
+++ Lab001.java
@@ -0,0 +1,37 @@
+class Lab001 {
+   
+
+
+use arraylist??
+
+
+        
+        quarterSales = new int[4];
+        private static corporateSales;
+
+        public static int quarterSalesCorporate(user input){
+            // take in 4 values
+            // put them in the array, in order?
+            corporateSales =+ quarter sales[0+1+2+3]
+        }
+
+        public static int subscript(index){
+             if(0<=index<=3)
+            return quarterSales[index];
+            else{
+                return -1
+            }
+        }
+
+        public static void main(){
+            System.out.println("Please enter the four quarterly sales for Div 0 separated by a space:");
+            System.out.println(user input);
+            System.out.println("Please enter the four quarterly sales for Div 1 separated by a space:");
+            System.out.println(user input);
+            System.out.println("Please enter the four quarterly sales for Div 2 separated by a space:");
+            System.out.println(user input);
+            System.out.println(table of info);
+            System.out.println("The total sales in the corporate year is : " + corporateSales );
+        }
+
+    }
