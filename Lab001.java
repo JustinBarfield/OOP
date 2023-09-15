d9d6db87d288ea22fad6a50c1740ae9243b7b502
JustinBarfield <122563454+JustinBarfield@users.noreply.github.com>
2023-09-15 16:31:42 +0000

    commit

--- Lab001.java
+++ Lab001.java
@@ -1,46 +1,48 @@
+import java. util.*;
 class Lab001 {
-   
+    private static int[] quarterlySales = new int[4]; // Define the array to store quarterly sales
 
         public static void  quarterSalesCorporate (){
-            private static double corporateSales = 0.0;
-            Scanner scanner = new Scanner(System.in);
-            int[] integers = new int[4];
-            int sum = 0;
-            String input = scanner.nextLine();
+            double corporateSales = 0.0;
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
         String[] inputArray = input.split(" ");
 
-        for (int x = 0; i < 4; x++) {
-            integers[x] = Integer.parseInt(inputArray[x]);
-            corporateSales += integers[x];
+        for (int x = 0; x < 4; x++) {
+            quarterlySales[x] = Integer.parseInt(inputArray[x]);
+            corporateSales += quarterlySales[x];
         }
 
+        System.out.println("Quarterly sales entered in order:");
         for (int i = 0; i < 4; i++) {
-            System.out.print(integers[i] + " ");
+            System.out.print(quarterlySales[i] + " ");
         }
 
-        System.out.println("The total sales in the corporate year is : " + corporateSales );
-         
-        }
-            
-            
+        System.out.println("\nThe total sales in the corporate year is: " + corporateSales);
         
-    
-
-        public static int subscript(int index){
-             if( 0 <= index && index <=3 )
-            return quarterSales[index];
-            else{
-                return -1;
-            }
-        }
-
-        public static void main(String[] args){
-            System.out.println("Please enter the four quarterly sales for Div 0 separated by a space:");
-            Lab001.quarterSalesCorporate();
-            
-           
-            System.out.println(table of info);
-            
-        }
-
     }
+
+
+
+    public static int subscript() {
+        System.out.println("Please enter the index you want to get: ");
+        Scanner sub = new Scanner(System.in);
+        int index = sub.nextInt();
+        if (0 <= index && index <= 3) {
+            return quarterlySales[index];
+        } else {
+            return -1; 
+        }
+    }
+
+       public static void main(String[] args) {
+        System.out.println("Please enter the four quarterly sales for Div 0 separated by a space:");
+        Lab001.quarterSalesCorporate();
+
+        
+        int indexToRetrieve = 2; 
+        int salesData = subscript(indexToRetrieve);
+        System.out.println("Quarterly sales at index " + indexToRetrieve + ": " + salesData);
+       
+    }
+}
