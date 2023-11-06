c32d6676145a1dc918862ebfc77dd4c8927c9192
Justin_Barfield <trumpet181006@gmail.com>
2023-11-06 13:42:51 -0600

    commit

---
+++ BrickBreaker/src/Bricks.java
@@ -0,0 +1,79 @@
+package src;
+
+import java.util.Vector;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Pair;
+
+public class Bricks extends Pane{
+    
+    int rows = 3, cols = 7;
+    //Rectangle bricks[][] = new Rectangle[rows][cols];
+    //int hit[][] = new int[rows][cols];
+    Vector<Rectangle> bricks = new Vector<>();
+    double brickX = 230, brickY = 100, brickWidth = 80, brickHeight = 30;
+
+    IntegerProperty scoreProperty = new SimpleIntegerProperty();
+
+    public Bricks(){
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                Rectangle rectangle = new Rectangle(brickX + j*brickWidth, brickY + i*brickHeight, brickWidth, brickHeight);
+                rectangle.setStroke(Color.WHITE);
+                getChildren().add(rectangle);
+                bricks.add(rectangle);
+                //hit[i][j] = 1;
+            }
+        }
+    }
+
+    public IntegerProperty scoreProperty(){
+        return scoreProperty;
+    }
+
+    public Pair<Boolean,String> intersects(Circle ball){
+        Pair<Boolean, String> result = new Pair<Boolean, String>(false, "");
+
+        for (Rectangle r : bricks){
+                    if (r.intersects(ball.getBoundsInLocal())){
+                        scoreProperty.setValue(scoreProperty.getValue() + 5);
+                        if (
+                            (ball.getCenterY() - ball.getRadius() <= r.getY() + r.getHeight() || 
+                            ball.getCenterY() + ball.getRadius() >= r.getY()) 
+                            && 
+                            (ball.getCenterX() >= r.getX() &&
+                            ball.getCenterX() <= r.getX() + r.getWidth())
+                        ) {
+                            result = new Pair<Boolean,String>(true, "y");
+                        }
+                        
+                        else if (
+                            (ball.getCenterX() - ball.getRadius() <= r.getX() + r.getWidth() || 
+                            ball.getCenterX() + ball.getRadius() >= r.getX()) 
+                            && 
+                            (ball.getCenterY() >= r.getY() &&
+                            ball.getCenterY() <= r.getY() + r.getHeight())
+                        ){
+                            result =  new Pair<Boolean,String>(true, "x");
+                        }
+                        else {
+                            result = new Pair<Boolean,String>(true, "y");
+                        }
+
+                        getChildren().remove(r);
+                        bricks.remove(r);
+                        //hit[i][j] = 0;
+                        return result;
+                    }
+                //}
+            
+        }
+        return result;
+    }
+
+}
---
+++ BrickBreaker/src/GamePane.java
@@ -0,0 +1,84 @@
+package src;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import javafx.util.Pair;
+
+public class GamePane extends Pane{
+
+    double ballX = 200, ballY = 400, radius = 20;
+    double dx = 1, dy = 1;
+    Circle ball = new Circle(ballX, ballY, radius);
+
+    double paddleX = 350, paddleY = 600, paddleWidth = 150, paddleHeight = 20;
+    double speed = 20;
+    Rectangle paddle = new Rectangle(paddleX, paddleY, paddleWidth, paddleHeight);
+
+    Timeline ballAnimation;
+
+    Bricks bricks = new Bricks();
+
+    public GamePane(){
+        getChildren().addAll(ball, paddle, bricks);
+        ballAnimation = new Timeline(new KeyFrame(new Duration(5), e->moveBall()));
+        ballAnimation.setCycleCount(Timeline.INDEFINITE);
+    }
+
+    public void movePaddle(KeyEvent e){
+        if (e.getCode() == KeyCode.LEFT){
+            if (paddleX > 0)
+                paddleX -= speed;
+        }
+        else if (e.getCode() == KeyCode.RIGHT){
+            if (paddleX + paddleWidth < getWidth())
+                paddleX += speed;
+        }
+        paddle.setX(paddleX);
+    }
+
+    public void play(){
+        ballAnimation.play();
+    }
+
+    public void moveBall(){
+        Pair<Boolean, String> result = bricks.intersects(ball);
+        if(ballY - radius < 0){
+            dy *= -1;
+        }
+        else if (ballX + radius > getWidth() || ballX - radius < 0){
+            dx *= -1;
+        }
+        else if (paddle.intersects(ball.getBoundsInLocal())){
+            dy *= -1;
+        }
+        else if (result.getKey() == true){
+            if (result.getValue().equals("y")){
+                dy *= -1;
+            }
+            else{
+                dx *= -1;
+            }
+        }
+
+        else if (ballY + radius > getHeight()){
+            ballAnimation.stop();
+        }
+
+        ballX += dx;
+        ballY += dy;
+        ball.setCenterX(ballX);
+        ball.setCenterY(ballY);
+    }
+
+    public IntegerProperty scoreProperty(){
+        return bricks.scoreProperty();
+    }
+    
+}
